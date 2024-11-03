721e44cdce39b4807040d02f11d8f860e601f2c2
ductm2205 <imoutstagram@gmail.com>
2024-11-03 16:53:28 +0700

    finalize

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
-require('dotenv').config
+require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.APP_HOST,
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.APP_HOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
